542d9df86a1da177c8661c0bc6c495ad8ff688ff
hojiakbaruralov293 <hojiakbaruralov293@gmail.com>
2026-03-16 09:39:49 +0500

    removing all hubs

--- src/InventoryManagementService.Web/Program.cs
+++ src/InventoryManagementService.Web/Program.cs
@@ -1,7 +1,6 @@
 using InventoryManagementService.Application;
 using InventoryManagementService.Infrastructure;
 using InventoryManagementService.Infrastructure.Data;
-//using InventoryManagementService.Web.Hubs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.DataProtection; // 1. Buni qo'shdik
@@ -76,6 +75,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//app.MapHub<CommentHub>("/hubs/comments");
+
 
 app.Run();
